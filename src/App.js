2577f464d6bf95aa116f35f83524ec7ab2d5c427
soons <soonso00@naver.com>
2024-11-14 21:57:25 +0900

    remove node_modules from tracking

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';  // Link 임포트 추가
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
-import Navbar from './components/Navbar'; // Navbar 컴포넌트 임포트
+import Navbar from './components/Navbar';
 
-// 임시 페이지 컴포넌트 정의
 function MemberService() {
-  return <div>회원 서비스 기능 준비중...</div>;
+  return (
+    <div className="MemberService">
+      회원 서비스 기능 준비중...
+    </div>
+  );
 }
 
 function About() {
-  return <div>About 페이지 준비중...</div>;
+  return (
+    <div className="About">
+      About 페이지 준비중...
+    </div>
+  );
 }
+
 
 function App() {
   const [file, setFile] = useState(null);
@@ -48,13 +56,11 @@ function App() {
           {file && <p>업로드된 파일: {file.name}</p>}
         </header>
 
-        {/* footer 수정 */}
         <footer className="App-footer">
           <Link to="/signup">회원가입</Link> | 
-          <Link to="/login"> 로그인</Link>  {/* 로그인 링크 추가 */}
+          <Link to="/login"> 로그인</Link>
         </footer>
 
-        {/* 라우팅 설정 */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignupForm />} />
@@ -70,7 +76,7 @@ function App() {
 function Home() {
   return (
     <div className="Home">
-      <h2>Welcome to our service!</h2>
+      <h2>클라우드컴퓨팅 5팀입니다.</h2>
     </div>
   );
 }
