ba08300693f44df0a0488b867955f4d8b0e936ce
soons <soonso00@naver.com>
2024-12-18 18:36:58 +0900

    env import 중복 삭제

--- backend/config/aws.js
+++ backend/config/aws.js
@@ -1,8 +1,7 @@
-const AWS = require('aws-sdk');
-require('dotenv').config();
+const aws = require('aws-sdk');
 
 // AWS 설정
-const s3 = new AWS.S3({
+const s3 = new aws.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
