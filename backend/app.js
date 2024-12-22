048a5331f02b957756d243b05356fd6739300fb4
soons <soonso00@naver.com>
2024-12-22 17:50:33 +0900

    merge를 위한 commit

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,6 @@ const path = require("path");
 
 const PORT = process.env.PORT;
 
-// // 빌드된 React 파일 제공 (정적 파일)
-// app.use(express.static(path.join(__dirname, "build")));
-// // 모든 라우트를 index.html로 리다이렉트
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
