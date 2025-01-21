ca7e864d23e7dd99f30037bc151852a9af87980d
soons <soonso00@naver.com>
2025-01-21 19:45:42 +0900

    webvital 삭제 및 build 설정 추가

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:5001", // 허용할 출처
-    methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 HTTP 메서드
-    credentials: true, // 쿠키와 인증 정보를 포함
-  })
-);
-// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000", // 허용할 출처
+//     methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 HTTP 메서드
+//     credentials: true, // 쿠키와 인증 정보를 포함
+//   })
+// );
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.use("/api", routes);
 
